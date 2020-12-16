217519cd2f644c04dfec340f29ce35b8e879aac7
shiohand <shiohand.0607@gmail.com>
2020-12-16 11:46:30 +0900

    update 20201216

--- JavaScript/jQuery/kyokasho/test5.js
+++ JavaScript/jQuery/kyokasho/test5.js
@@ -15,10 +15,70 @@ jQuery(function($) {
     });
 
     function moveHighlight(event, ui) { // event, オブジェクト
-      const $newTab = ui.navTab || ui.tab; // 新しく選択されたタブ
+      const $newTab = ui.newTab || ui.tab; // 新しく選択されたタブ
       const left = $newTab.position().left; // left
       // ハイライトをleftへアニメーション
       $highlight.animate({ left: left }, 500, 'easeOutExpo');
     }
   });
+
+  // スクロールトップ
+  $('.back-to-top').each(function() {
+    // scrollableElement()でスクロール操作をどっちでできるか検出
+    const el = scrollableElement('html', 'body');
+    // クリックイベント設定
+    $(this).on('click', function(event) {
+      event.preventDefault();
+      $(el).animate({scrollTop: 0}, 250);
+    });
+  });
+
+  // scrollableElement()
+  // 別に覚えなくてもぐぐればでてくるよ
+  function scrollableElement(...elms) {
+    for(elm of elms) {
+      $elm = $(elm);
+      if ($elm.scrollTop() > 0) {
+        // 既に稼働中(ある) 返す
+        return elm;
+      } else {
+        // 0なのかないのか
+        $elm.scrollTop(1); // 1にしてみる
+        const scrollable = $el.scrollTop() > 0; // どうだ？
+        $elm.scrollTop(0); // ちゃんと戻しとく
+        if (scrollable) {
+          // trueだ 1になっていた 返す
+          return elm;
+        }
+      }
+    }
+    return []; // 該当なし
+  }
 });
+
+/**
+ * jQuery UI Tabs
+ * 
+ * タブ と パネル を含む
+ * href と id で関連付けると切り替えを実装してくれる
+ * 
+ * 今回は
+ * <section class="work-section">
+ *   <ul>
+ *     <li><a href="#work01">Work 01</a></li>
+ *   </ul>
+ *   <section id="work01"></section>
+ * </section>
+ * という感じ
+ * 
+ * オプション 今回使用したもの
+ * hide: { duration: 250 }
+ * show: { duration: 125 }
+ * create: 読込時 関数
+ * activate: 選択時 関数
+ *   createやactiveなどのUI Tabsから呼ばれる関数は、引数に(event, ui)を受け取る
+ *   event  UI Tabs独自のイベントオブジェクト
+ *   ui     発生元要素の情報を持つオブジェクト
+ *          ui.tabs     該当タブ(createのとき)
+ *          ui.newTabs  該当タブ(activateのとき)
+ */
